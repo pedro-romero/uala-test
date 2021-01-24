070a45bda76dea0e83b7bc60a42bdcec90b75dbc
Pedro Ivan Romero Ojeda <pedro128@gmail.com>
2021-01-23 19:33:49 -0600

    Add meal detail screen

---
+++ UalaTest/Views/MealViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MealViewController.swift
+//  UalaTest
+//
+//  Created by Pedro Iván Romero Ojeda on 1/23/21.
+//  Copyright © 2021 piro. All rights reserved.
+//
+
+import UIKit
+
+class MealViewController: UIViewController {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var ingredientsStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
